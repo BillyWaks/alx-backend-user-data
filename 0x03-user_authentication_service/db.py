0ce381019828bea07bbaa2098c573bb58337570c
“BillyWaks” <136112711+BillyWaks@users.noreply.github.com>
2024-09-24 20:44:25 +0300

    database

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""DB module
+"""DB module.
 """
 from sqlalchemy import create_engine, tuple_
 from sqlalchemy.exc import InvalidRequestError
@@ -10,44 +10,39 @@ from sqlalchemy.orm.session import Session
 
 from user import Base, User
 
-
-# Define the base class for all models
-Base = declarative_base()
-
-
 class DB:
-    """DB class
+    """DB class.
     """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
+        """Initialize a new DB instance.
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
+        """Memoized session object.
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email: str, hashed_password: str) -> User:
         """Adds a new user to the database.
         """
         try:
             new_user = User(email=email, hashed_password=hashed_password)
             self._session.add(new_user)
-            self.__session.commit()
+            self._session.commit()
         except Exception:
-            self.__session.rollback()
+            self._session.rollback()
             new_user = None
         return new_user
-    
+
     def find_user_by(self, **kwargs) -> User:
         """Finds a user based on a set of filters.
         """
